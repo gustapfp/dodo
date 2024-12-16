4b54e8df8ff655714bb06b8d26103b112724c796
gustavo.pfp <gustavopfp.trab@gmail.com>
2024-12-16 19:41:45 -0300

    core: database settings update

--- core/settings.py
+++ core/settings.py
@@ -88,11 +88,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
-}
+ }
 
 
 # Password validation
