3e497849881def5c2da67c5c87fa8e03ccef688e
gustapfp <gustavopfp.trab@gmail.com>
2024-12-22 12:28:24 -0300

    feat: tests for user creation

--- core/settings.py
+++ core/settings.py
@@ -90,12 +90,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
-    }
+    },
+    'TEST': {
+            'NAME': 'TESTDB',  # Test database name
+        },
  }
 
 
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    def test_user_creation(self):
+        UserTable = get_user_model()
+        user = UserTable.objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="test12345678"
+        )
+
+        self.assertEqual(user.username, "test")
+        self.assertEqual(user.email, "test@test.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_superuser_creation(self):
+        UserTable = get_user_model()
+        superuser = UserTable.objects.create_superuser(
+            username="test",
+            email="test@test.com",
+            password="test12345678"
+        ) 
+        self.assertEqual(superuser.username, "test")
+        self.assertEqual(superuser.email, "test@test.com")
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+
+    
