2cb414d6e588e91851302439fbacd4758a23993b
gustapfp <gustavopfp.trab@gmail.com>
2025-01-07 18:08:58 -0300

    feat: add a sectors field in Hospital model

--- data_management/models.py
+++ data_management/models.py
@@ -2,23 +2,9 @@ from django.db import models
 from django.db.models import ForeignKey
 
 
-# class Hospital(models.Model):
-#     name = models.CharField(max_length=80)
-#     address = models.CharField(max_length=80)
-#     phone = models.CharField(max_length=80)
-#     email = models.EmailField(max_length=80)
-#     description = models.TextField()
+class Evaluator(models.Model):
+    name = models.CharField(max_length=80)
+    evaluation_date = models.DateTimeField(auto_now_add=True)
 
-# class HospitalSector(models):
-#     hospital = ForeignKey(Hospital)
-
-# # Create your models here.
-# class Sorting(models.Model):
-
-#     hospitals = Hospital.objects.all()
-#     hospital_choices = [hospital.name for hospital in hospitals]
-
-#     hospital = models.CharField(
-#         max_length=80, 
-#         choices=hospital_choices
-#         )
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractBaseUser
-from django.db.models import fields
 from django.db import models
 
 class User(AbstractBaseUser):
@@ -9,11 +8,22 @@ class User(AbstractBaseUser):
     #     help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
     # )
 
+
+class Sector(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Hospital(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
     email = models.EmailField(max_length=80)
     contact_number = models.TextField(max_length=16)
     address = models.TextField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     active_user = models.BooleanField(default=True)
+    sectors = models.ManyToManyField(Sector, related_name='Hospitals', blank=True)
     # last_service = models.ForeignKey
+
+    def __str__(self):
+        return self.name
