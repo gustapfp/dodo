86e40a09cb278e36d2fc850d76ba24dd0b478893
gustapfp <gustavopfp.trab@gmail.com>
2025-05-19 11:34:29 -0300

    fix: credentials bug

--- core/settings.py
+++ core/settings.py
@@ -187,7 +187,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # Your email address
-EMAIL_HOST_PASSWORD = "kpec qpkp plkw bweo" # Your email password or app-specific password
+EMAIL_HOST_PASSWORD = "lius axqr rwzy nkqs" # Your email password or app-specific password
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
